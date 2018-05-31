cf52134a97a1dbadd6bf941fbcd833c890f85d4c
Connor Rigby <konnorrigby@gmail.com>
2018-05-31 08:14:43 -0700

    A wild Problem has appeared

--- lib/nerves_network_ng/raw_config.ex
+++ lib/nerves_network_ng/raw_config.ex
@@ -1,7 +1,27 @@
 defmodule Nerves.NetworkNG.RawConfig do
-  defstruct []
+  alias Nerves.NetworkNG.RawConfig
+  defstruct [
+    :interfaces,
+    :wpa_supplicant,
+    :udhcpd,
+    :reload
+  ]
   @type ifname :: String.t
-  @type t :: %__MODULE__{}
+  @type t :: %RawConfig{}
+
+  @interfaces_path Path.join(["etc", "network", "interfaces"])
+  @wpa_supplicant_path Path.join(["etc", "wpa_supplicant.conf"])
+  @udhcpd_path Path.join(["etc", "udhcpd.conf"])
+
+  def write(%RawConfig{} = conf) do
+    File.write!(@interfaces_path, conf.interfaces)
+    File.write!(@wpa_supplicant_path, conf.wpa_supplicant)
+    File.write!(@udhcpd_path, conf.udhcpd_path)
+  end
+
+  def apply(%RawConfig{} = conf) do
+    # essentially something like: System.cmd("ifup", conf.reload) or something.
+  end
 end
 
 defprotocol ToRawConfig do
